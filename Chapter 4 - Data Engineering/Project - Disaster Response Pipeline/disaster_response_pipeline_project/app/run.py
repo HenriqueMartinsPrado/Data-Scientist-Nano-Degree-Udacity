20ea52b6b6e09f9a1cf4955654a45391dfa7f712
HenriqueMartinsPrado <hmartins.prado@gmail.com>
2021-01-20 09:46:07 -0300

    Updating files about the course

--- Chapter 4 - Data Engineering/Project - Disaster Response Pipeline/disaster_response_pipeline_project/app/run.py
+++ Chapter 4 - Data Engineering/Project - Disaster Response Pipeline/disaster_response_pipeline_project/app/run.py
@@ -29,11 +29,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/hmp_DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('FigureEight', engine)
 
 # load model
-model = joblib.load("../models/hmp_ModelClassifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
